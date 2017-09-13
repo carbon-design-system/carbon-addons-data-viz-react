52039f87256f81f866e0f2b3fb082ba62fa3081b
TJ Egan <twegan@us.ibm.com>
2017-09-13 11:25:44 -0500

    chore(build): add semantic release

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export DetailPageSidebar from './components/DetailPageSidebar/DetailPageSidebar.js';
-export DetailPageHighlights from './components/DetailPageHighlights/DetailPageHighlights.js';
+// export DetailPageSidebar from './components/DetailPageSidebar/DetailPageSidebar.js';
+// export DetailPageHighlights from './components/DetailPageHighlights/DetailPageHighlights.js';
