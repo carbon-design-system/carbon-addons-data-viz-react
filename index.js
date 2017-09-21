f5af49d936f0ff76af998eabd64d154144e07ca0
TJ Egan <twegan@us.ibm.com>
2017-09-21 13:44:09 -0500

    fix(pie-chart): export PieChart

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export LineGraph from './components/LineGraph/LineGraph.js';
 export GaugeGraph from './components/GaugeGraph/GaugeGraph.js';
 export ScatterPlot from './components/ScatterPlot/ScatterPlot.js';
+export PieChart from './components/PieChart/PieChart.js';
