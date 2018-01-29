1b92a08408a79a8eeccdc956c0cde4208d676ff5
TJ Egan <twegan@us.ibm.com>
2018-01-29 11:01:49 -0600

    chore(export): add BarGraph to exports

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+export BarGraph from './components/BarGraph/BarGraph.js';
 export LineGraph from './components/LineGraph/LineGraph.js';
 export GaugeGraph from './components/GaugeGraph/GaugeGraph.js';
 export ScatterPlot from './components/ScatterPlot/ScatterPlot.js';
