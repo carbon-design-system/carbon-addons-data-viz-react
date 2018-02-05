8862813a056f4e9ee1a827a3f95f602a3e083099
TJ Egan <twegan@us.ibm.com>
2018-02-05 17:02:28 -0600

    feat(data-tooltip): add some styles, refactor component

--- components/DataTooltip/DataTooltip.js
+++ components/DataTooltip/DataTooltip.js
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 const propTypes = {
   className: PropTypes.string,
   data: PropTypes.array,
+  direction: PropTypes.string,
   id: PropTypes.string,
   heading: PropTypes.string,
   isActive: PropTypes.bool,
@@ -12,8 +13,9 @@ const propTypes = {
 
 const defaultProps = {
   data: [100],
+  direction: 'top',
   id: 'bx--data-tooltip',
-  heading: 'Heading',
+  heading: null,
   isActive: true,
 };
 
@@ -32,10 +34,33 @@ class DataTooltip extends Component {
   renderTooltipData() {
     const { data } = this.props;
     const items = data.map(item => {
+      let divStyle;
+      if (item.color) {
+        if (data.length <= 1) {
+          divStyle = {
+            borderTop: `4px solid ${item.color}`,
+            minHeight: '2.625rem',
+            padding: '0 0.625rem',
+          };
+        } else {
+          divStyle = {
+            borderLeft: `4px solid ${item.color}`,
+            minHeight: '2rem',
+          };
+        }
+      } else {
+        divStyle = {
+          minHeight: '1.625rem',
+          padding: '0 0.625rem',
+        };
+      }
+
       return (
-        <li className="bx--tooltip-list-item">
-          <span>{item}</span>
-          <span>Test</span>
+        <li style={divStyle} className="bx--tooltip-list-item">
+          {item.label && (
+            <span className="bx--tooltip-list-item__label">{item.label}</span>
+          )}
+          <span className="bx--tooltip-list-item__data">{item.data}</span>
         </li>
       );
     });
@@ -44,10 +69,11 @@ class DataTooltip extends Component {
   }
 
   render() {
-    const { heading, isActive, className } = this.props;
+    const { className, direction, heading, isActive } = this.props;
 
     const tooltipClasses = classNames(
       'bx--tooltip',
+      'bx--data-tooltip',
       {
         'bx--tooltip--shown': isActive,
       },
@@ -55,8 +81,8 @@ class DataTooltip extends Component {
     );
 
     return (
-      <div className={tooltipClasses} data-floating-menu-direction="right">
-        <p className="bx--tooltip__label">{heading}</p>
+      <div className={tooltipClasses} data-floating-menu-direction={direction}>
+        {heading && <p className="bx--tooltip__label">{heading}</p>}
         <ul className="bx--tooltip-list">{this.renderTooltipData()}</ul>
       </div>
     );
--- components/DataTooltip/DataTooltip.story.js
+++ components/DataTooltip/DataTooltip.story.js
@@ -2,10 +2,69 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import DataTooltip from './DataTooltip';
 
-storiesOf('DataTooltip', module).addWithInfo(
-  'Default',
-  `
+const singleNoColorData = [{ data: '$250.17' }];
+
+const singleData = [{ data: '$250.17', label: 'Jan', color: '#3b1a40' }];
+
+const tripleData = [
+  { data: '$123.45', label: 'Jan', color: '#3b1a40' },
+  { data: '$112.22', label: 'Feb', color: '#473793' },
+  { data: '$250.17', label: 'Mar', color: '#3c6df0' },
+];
+
+const quadData = [
+  { data: '$123.45', label: 'Jan', color: '#3b1a40' },
+  { data: '$112.22', label: 'Feb', color: '#473793' },
+  { data: '$250.17', label: 'Mar', color: '#3c6df0' },
+  { data: '$123.45', label: 'Apr', color: '#00a68f' },
+];
+
+const maxData = [
+  { data: '$123.45', label: 'Jan', color: '#3b1a40' },
+  { data: '$112.22', label: 'Feb', color: '#473793' },
+  { data: '$250.17', label: 'Mar', color: '#3c6df0' },
+  { data: '$123.45', label: 'Apr', color: '#00a68f' },
+  { data: '$112.22', label: 'May', color: '#48d4bb' },
+  { data: '$250.17', label: 'Jun', color: '#9b82f3' },
+];
+
+const props = {
+  heading: 'January',
+};
+
+storiesOf('DataTooltip', module)
+  .addWithInfo(
+    'Default',
+    `
+    Data Tooltip.
+  `,
+    () => <DataTooltip data={singleNoColorData} />
+  )
+  .addWithInfo(
+    'Single',
+    `
       Data Tooltip.
     `,
-  () => <DataTooltip />
-);
+    () => <DataTooltip data={singleData} />
+  )
+  .addWithInfo(
+    'Triple',
+    `
+      Data Tooltip.
+    `,
+    () => <DataTooltip data={tripleData} {...props} />
+  )
+  .addWithInfo(
+    'Quad',
+    `
+      Data Tooltip.
+    `,
+    () => <DataTooltip data={quadData} {...props} />
+  )
+  .addWithInfo(
+    'Max',
+    `
+      Data Tooltip.
+    `,
+    () => <DataTooltip data={maxData} {...props} />
+  );
