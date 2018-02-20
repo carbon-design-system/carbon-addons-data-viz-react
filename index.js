acc8c09fe9847164b6d1fe6e51d3b29ef430bbc9
TJ Egan <twegan@us.ibm.com>
2018-02-20 10:41:14 -0600

    fix(index): export DataTooltip

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export LineGraph from './components/LineGraph/LineGraph.js';
 export GaugeGraph from './components/GaugeGraph/GaugeGraph.js';
 export ScatterPlot from './components/ScatterPlot/ScatterPlot.js';
 export PieChart from './components/PieChart/PieChart.js';
+export DataTooltip from './components/DataTooltip/DataTooltip';
