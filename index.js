401645a31a8f9c4a5efc877aa8f158c5bcf3a0c0
Liviu Dobrea <dobrealiviuc@gmail.com>
2018-05-25 00:17:35 +0300

    chore(negative-bar-chart): export default

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export BarGraph from './components/BarGraph/BarGraph.js';
-export BarGraphHorizontal from './components/BarGraphHorizontal/BarGraphHorizontal';
+export BarGraphHorizontal from './components/BarGraphHorizontal/BarGraphHorizontal.js';
+export BarGraphNegative from './components/NegativeBarGraph/NegativeBarGraph.js';
 export LineGraph from './components/LineGraph/LineGraph.js';
 export GaugeGraph from './components/GaugeGraph/GaugeGraph.js';
 export ScatterPlot from './components/ScatterPlot/ScatterPlot.js';
